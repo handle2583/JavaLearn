a72145159ee244c6b2b19ecfa14143424ad4b053
handle2583 <1716617301@qq.com>
2020-06-12 21:12:34 +0800

    滴滴的日常学习20200612

---
+++ src/com/didi/concurrency/FixedThreadPool.java
@@ -0,0 +1,24 @@
+package com.didi.concurrency;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class FixedThreadPool {
+
+    public static void main(String[] args){
+
+
+        ExecutorService executorService = Executors.newFixedThreadPool(4) ;
+
+
+        for (int i = 0 ; i< 5 ;i++){
+
+            executorService.execute(new LiftOff());
+
+        }
+        executorService.shutdown();
+
+
+    }
+}
---
+++ src/com/didi/concurrency/LiftOff.java
@@ -0,0 +1,34 @@
+package com.didi.concurrency;
+
+public class LiftOff implements Runnable {
+        protected  int countDown = 10 ;
+
+        private int taskCount = 0 ;
+        private  final  int id = taskCount++ ;
+        public LiftOff(){
+
+        }
+
+        public LiftOff(int countDown) {
+            this.countDown = countDown ;
+        }
+
+  public String status(){
+
+            return "#" +id + "("
+
+                    +(countDown >0 ?countDown : "LiftOff") +")" ;
+
+  }
+    @Override
+    public void run() {
+        while(countDown --> 0){
+
+            System.out.println(status());
+            Thread.yield();
+
+        }
+
+
+    }
+}
