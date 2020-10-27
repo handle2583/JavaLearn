b535a31eadf578671ed523792e21e1cd7101860a
miaojian_v <miaojian_v@didichuxing.com>
2020-10-27 18:59:03 +0800

    20201027 疯狂java讲义

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
 
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(Main.test());
     }
-}
+
+    static int test(){
+
+
+        int x = 1 ;
+        try{
+            return x ;
+        }finally {
+            System.out.println("finally");
+            ++x ;
+        }
+    }
+   }
